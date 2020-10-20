fc15c4f1bf2d966bf9577478b31361bcb7ba420b
Volodymyr Vyshnenko <vyshnenko2904@gmail.com>
2020-10-20 22:24:44 +0300

    Добавьте файлы проекта.

---
+++ 6/Converter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _6
+{
+    internal class Converter
+    {
+        public double USD { get; set; }
+        public double EUR { get; set; }
+        public double RUB { get; set; }
+        public double GRN { get; set; }
+
+
+        public Converter()
+        {
+            USD = 0;
+            EUR = 0;
+            RUB = 0;
+        }
+        public Converter(double usd, double eur, double rub)
+        {
+            USD = usd;
+            EUR = eur;
+            RUB = rub;
+        }
+
+        public double Convert(int currency, double amount)
+        {
+            switch (currency)
+            {
+                //dollar to hryvna
+                case 1:
+                    return amount * USD;
+                //euro to hryvna
+                case 2:
+                    return amount * EUR;
+                //ruble to hryvna
+                case 3:
+                    return amount * RUB;
+                //hryvna to dollar
+                case 4:
+                    return amount / USD;
+                //hryvna to euro
+                case 5:
+                    return amount / EUR;
+                //hryvna to ruble
+                case 6:
+                    return amount / RUB;
+                default:
+                    Console.WriteLine("Вы ввели некорректное значение!");
+                    return -1;
+
+
+            }
+        }
+    }
+}
---
+++ 6/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какой сегодня курс валюты? Запишите через пробел доллар к гривне, евро к гривне, рубль к гривне:");
+            string[] inp = Console.ReadLine().Split(' '); double[] input = new double[] { }; int counter = 0;
+            if (inp != null && inp.Length == 3)
+            {
+                Converter conv = new Converter(Convert.ToDouble(inp[0]), Convert.ToDouble(inp[1]), Convert.ToDouble(inp[2]));
+                Console.WriteLine("Вы ходите конвертировать гривну к какой-то валюте(1) или какую-то валюту к гривне(2)?");
+                int decision = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите сумму:");
+                double amount = Convert.ToDouble(Console.ReadLine());
+                if (decision == 1)
+                {
+                    Console.WriteLine("Конвертировать к доллару(1), к евро(2), к рублю(3):");
+                    int info = Convert.ToInt32(Console.ReadLine());
+                    if (info == 1)
+                    {
+                        Console.WriteLine(conv.Convert(4, amount));
+                    }
+                    else if (info == 2)
+                    {
+                        Console.WriteLine(conv.Convert(5, amount));
+                    }
+                    else if (info == 3)
+                    {
+                        Console.WriteLine(conv.Convert(6, amount));
+                    }
+                    else Console.WriteLine("Некорректная информация.");
+                }
+                else if (decision == 2)
+                {
+                    Console.WriteLine("Конвертировать доллар к гривне(1), евро(2) или рубль(3):");
+                    int info = Convert.ToInt32(Console.ReadLine());
+                    if (info == 1)
+                    {
+                        Console.WriteLine(conv.Convert(1, amount));
+                    }
+                    else if (info == 2)
+                    {
+                        Console.WriteLine(conv.Convert(2, amount));
+                    }
+                    else if (info == 3)
+                    {
+                        Console.WriteLine(conv.Convert(3, amount));
+                    }
+                    else Console.WriteLine("Некорректная информация.");
+                }
+                else Console.WriteLine("Некорректная информация.");
+                Console.ReadKey();
+            }
+
+            else { Console.WriteLine("Некорректное значение."); Environment.Exit(0); }
+        }
+    }
+}
